28abe2112f79db38feb3835db9bccbd0ef15b63b
Oriirivas <orianarivas7@gmail.com>
2020-08-16 11:51:40 -0400

    my firts commit

---
+++ src/test/java/com/testLatam/orianaRivas/imp/PersonImpTest.java
@@ -0,0 +1,14 @@
+package com.testLatam.orianaRivas.imp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonImpTest {
+    @Test
+    public void testUnit() {
+
+    }
+
+}
